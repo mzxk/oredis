6bd9036c21f8d6f455bcbf78c47ddb5c55fe3b35
nono <catjuicy@gmail.com>
2020-07-26 00:22:03 +0800

    增加支持了script

--- oredis.go
+++ oredis.go
@@ -7,9 +7,34 @@ import (
 )
 
 type Oredis struct {
-	pool *redis.Pool
+	pool   *redis.Pool
+	script map[string]string
 }
 
+//Script 这个函数保存对应的名字和lua脚本到redis服务器，并缓存hash在本地
+func (t *Oredis) Script(name, lua string) error {
+	c := t.Get()
+	defer c.Close()
+	hash, err := redis.String(c.Do("script", "load", lua))
+	if err != nil {
+		return err
+	}
+	t.script[name] = hash
+	return nil
+}
+
+//Eval 这个函数将调用本地缓存的对应名字的hash来调用lua脚本
+func (t *Oredis) Eval(name string, keys []interface{}, args ...interface{}) (interface{}, error) {
+	ags := []interface{}{
+		t.script[name],
+		len(keys),
+	}
+	ags = append(ags, keys...)
+	ags = append(ags, args...)
+	c := t.Get()
+	defer c.Close()
+	return c.Do("evalsha", ags...)
+}
 func (t *Oredis) Get() redis.Conn {
 	conn := t.pool.Get()
 	return conn
@@ -51,6 +76,6 @@ func New(add, pwd string) *Oredis {
 		// then Get() waits for a connection to be returned to the pool before returning
 		Wait: true,
 	}
-	t := &Oredis{pool: p}
+	t := &Oredis{pool: p, script: map[string]string{}}
 	return t
 }
